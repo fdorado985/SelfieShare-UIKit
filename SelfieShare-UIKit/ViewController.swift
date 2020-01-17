93735fd9058a3d5a995cc2dd5a264685ccae83ae
Juan Dorado <fdorado985@gmail.com>
2020-01-17 02:07:23 -0600

    Initial commit

---
+++ SelfieShare-UIKit/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SelfieShare-UIKit
+//
+//  Created by Juan Francisco Dorado Torres on 17/01/20.
+//  Copyright © 2020 Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
