b873cce28a40ec2e49b8678c8e17fbe7a80cf3d8
Juan Dorado <fdorado985@gmail.com>
2020-01-17 23:38:54 -0600

    importing-photos

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 
 ## Description
 Make a multipeer photo sharing app in just 150 lines of code.
+
+## Tags
+This app has been worked through sections, so if you want to see an specific section, you can go through tags to take a look at them.
+
+### importing-photos
+| Screenshot 01 | Screenshot 02 |
+| ------------- | ------------- |
+| ![ss01](.screenshots/ss01.png) | ![ss02](.screenshots/ss02.png) |
+
+| Screenshot 03 | Screenshot 04 |
+| ------------- | ------------- |
+| ![ss03](.screenshots/ss03.png) | ![ss04](.screenshots/ss04.png) |
--- SelfieShare-UIKit/ViewController.swift
+++ SelfieShare-UIKit/ViewController.swift
@@ -8,10 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
+
+  // MARK: - Properties
+
+  var images = [UIImage]()
+
+  // MARK: - View lifecycle apply_to_dictionaries: true
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    title = "Selfie Share"
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+  }
+
+  // MARK: - Methods
+
+  @objc func importPicture() {
+    let picker = UIImagePickerController()
+    picker.allowsEditing = true
+    picker.delegate = self
+    present(picker, animated: true)
+  }
+}
+
+// MARK: - UINavigationController & UIImagePickerController delegates
+
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+    guard let image = info[.editedImage] as? UIImage else { return }
+
+    dismiss(animated: true)
+
+    images.insert(image, at: 0)
+    collectionView.reloadData()
+  }
+}
+
+// MARK: - UICollectionView delegates
+
+extension ViewController {
+
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return images.count
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+
+    if let imageView = cell.viewWithTag(1000) as? UIImageView {
+      imageView.image = images[indexPath.item]
+    }
+
+    return cell
   }
 }
